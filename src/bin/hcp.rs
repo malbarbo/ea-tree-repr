c6fbe7b40f3b12d241333780ea1d83b174232798
Marco A L Barbosa <malbarbo@gmail.com>
2018-04-03 14:16:38 -0300

    Overall improvements to src/bin/hcp.rs
    - Add args: pop_size, max_iters, seed, etc
    - Transformation from hcp to hpp
    - Option to choose between path or cycle
    - Avoid adding repeated individuals to the population
    - Use (branches, leafs) as fitness
    - Add quiet mode

--- src/bin/hcp.rs
+++ src/bin/hcp.rs
@@ -8,16 +8,24 @@ extern crate clap;
 
 use ea_tree_repr::*;
 
+use clap::ErrorKind;
 use fera::fun::{position_max_by_key, position_min_by_key, vec};
-use fera::graph::algs::Kruskal;
+use fera::graph::algs::{Kruskal, Paths};
 use fera::graph::prelude::*;
-use rand::Rng;
+use fera::graph::traverse::{Dfs, OnDiscoverTreeEdge};
+use rand::{Rng, SeedableRng};
 
+use std::collections::BTreeMap;
 use std::rc::Rc;
 
 pub fn main() {
-    let (repr, op, input) = args();
-    let instance = tsplib::read(input).expect("Fail to parse input file");
+    let args = args();
+
+    if !args.quiet {
+        println!("{:#?}", args);
+    }
+
+    let instance = tsplib::read(&args.input).expect("Fail to parse input file");
 
     assert_eq!(
         Some(tsplib::Type::Hcp),
@@ -35,40 +43,79 @@ pub fn main() {
         panic!("Only EDGE_DATA_FORMAT : EDGE_LIST is supported")
     }
 
-    let g = Rc::new(builder.finalize());
+    let g = builder.finalize();
+    let g = if args.path {
+        Rc::new(g)
+    } else {
+        Rc::new(transform(&g))
+    };
+    let g = &g;
 
-    match repr {
-        Repr::EulerTour => {
-            run::<EulerTourTree<_>, CowNestedArrayVertexProp<StaticGraph, u32>>(g, op)
-        }
-        Repr::NddrAdj => run::<NddrAdjTree<_>, CowNestedArrayVertexProp<StaticGraph, u32>>(g, op),
-        Repr::NddrBalanced => {
-            run::<NddrBalancedTree<_>, CowNestedArrayVertexProp<StaticGraph, u32>>(g, op)
-        }
-        Repr::Predecessor => {
-            run::<PredecessorTree<_>, DefaultVertexPropMut<StaticGraph, u32>>(g, op)
-        }
-        Repr::Predecessor2 => {
-            run::<PredecessorTree2<_>, CowNestedArrayVertexProp<StaticGraph, u32>>(g, op)
+    let mut count = BTreeMap::new();
+    for u in g.vertices() {
+        *count.entry(g.out_degree(u)).or_insert(0) += 1;
+    }
+
+    if !args.quiet {
+        println!(
+            "graph size\n  n = {}\n  m = {}",
+            g.num_vertices(),
+            g.num_edges()
+        );
+        println!("degree distribution");
+        for (d, c) in count {
+            println!("{:2} = {:2}", d, c);
         }
     }
+
+    let edges = match args.repr {
+        Repr::EulerTour => {
+            run::<EulerTourTree<_>, CowNestedArrayVertexProp<StaticGraph, u32>>(g, &args).edges()
+        }
+        Repr::NddrAdj => {
+            run::<NddrAdjTree<_>, CowNestedArrayVertexProp<StaticGraph, u32>>(g, &args).edges()
+        }
+        Repr::NddrBalanced => {
+            run::<NddrBalancedTree<_>, CowNestedArrayVertexProp<StaticGraph, u32>>(g, &args).edges()
+        }
+        Repr::Predecessor => {
+            run::<PredecessorTree<_>, DefaultVertexPropMut<StaticGraph, u32>>(g, &args).edges()
+        }
+        Repr::Predecessor2 => {
+            run::<PredecessorTree2<_>, CowNestedArrayVertexProp<StaticGraph, u32>>(g, &args).edges()
+        }
+    };
+
+    let mut path = path(g, &edges);
+
+    if !args.path {
+        transform_path(g, &mut path);
+    }
+
+    print!("{} ", args.input);
+    let mut space = "";
+    for u in path.iter().map(|&e| g.source(e)) {
+        print!("{}{}", space, u + 1);
+        space = " ";
+    }
+    println!("{}{}", space, g.target(*path.last().unwrap()) + 1);
 }
 
-fn run<T, D>(g: Rc<StaticGraph>, op: Op)
+fn run<T, D>(g: &Rc<StaticGraph>, args: &Args) -> T
 where
     T: Tree<StaticGraph>,
     D: VertexPropMutNew<StaticGraph, u32>,
     Ind<T, D>: Clone,
 {
-    let mut rng = rand::weak_rng();
+    let mut rng = rand::XorShiftRng::from_seed([args.seed, args.seed, args.seed, args.seed]);
     let mut edges = vec(g.edges());
     let mut tree = vec![];
     let mut pop = vec![];
-    for _ in 0..5 {
+    for _ in 0..args.pop_size {
         rng.shuffle(&mut edges);
         tree.clear();
         tree.extend(g.kruskal().edges(&edges));
-        if pop.len() == 0 {
+        if pop.is_empty() {
             pop.push(Ind::<T, D>::new(
                 g.clone(),
                 &tree,
@@ -81,19 +128,33 @@ where
         }
     }
 
-    println!("n = {}, m = {}", g.num_vertices(), g.num_edges());
-
     let mut best = position_min_by_key(&pop, |ind| ind.fitness()).unwrap();
-    println!("best = {:?}", pop[best].value());
-    for it in 0.. {
+
+    if pop[best].is_optimum() {
+        return pop[best].clone().tree;
+    }
+
+    if !args.quiet {
+        println!("best = {:?}", pop[best].value());
+    }
+
+    let mut last_it_impr = 0;
+    for it in 0..=args.max_num_iters.unwrap_or(u64::max_value()) {
         let i = rng.gen_range(0, pop.len());
         let mut new = pop[i].clone();
-        new.mutate(op, &mut rng);
+        new.mutate(args.op, &mut rng);
+        if pop.contains(&new) {
+            continue;
+        }
         if new.fitness() <= pop[i].fitness() {
             if new.fitness() < pop[best].fitness() {
+                last_it_impr = it;
                 best = i;
-                println!("it = {}, best = {:?}", it, new.value());
-                if new.branches == 0 {
+                if !args.quiet {
+                    println!("it = {}, best = {:?}", it, new.value());
+                }
+                if new.is_optimum() {
+                    pop[i] = new;
                     break;
                 }
             }
@@ -103,56 +164,18 @@ where
             pop[j] = new;
             best = position_min_by_key(&pop, |ind| ind.fitness()).unwrap();
         }
+
+        if it - last_it_impr >= args.max_num_iters_no_impr.unwrap_or(u64::max_value()) {
+            if !args.quiet {
+                println!(
+                    "max number of iterations without improvement reached: {:?}",
+                    args.max_num_iters_no_impr
+                );
+            }
+            break;
+        }
     }
-}
-
-fn args() -> (Repr, Op, String) {
-    let app = clap_app!(
-        ("hcp") =>
-            (version: crate_version!())
-            (about: crate_description!())
-            (author: crate_authors!())
-            (@arg repr:
-                +required
-                possible_values(&[
-                    "euler-tour",
-                    "nddr-adj",
-                    "nddr-balanced",
-                    "pred",
-                    "pred2",
-                ])
-                "tree representation"
-            )
-            (@arg op:
-                +required
-                possible_values(&[
-                    "change-pred",
-                    "change-any"
-                ])
-                "operation"
-            )
-            (@arg input:
-                +required
-                "input file graph (an HCP tsplib instance)"
-            )
-    );
-
-    let matches = app.get_matches();
-    let repr = match matches.value_of("repr").unwrap() {
-        "euler-tour" => Repr::EulerTour,
-        "nddr-adj" => Repr::NddrAdj,
-        "nddr-balanced" => Repr::NddrBalanced,
-        "pred" => Repr::Predecessor,
-        "pred2" => Repr::Predecessor2,
-        _ => unreachable!(),
-    };
-    let op = match matches.value_of("op").unwrap() {
-        "change-pred" => Op::ChangePred,
-        "change-any" => Op::ChangeAny,
-        _ => unreachable!(),
-    };
-
-    (repr, op, matches.value_of("input").unwrap().to_string())
+    pop[best].clone().tree
 }
 
 #[derive(Clone)]
@@ -163,8 +186,19 @@ where
 {
     tree: T,
     degree: D,
+    hash: usize,
     branches: u32,
     leafs: u32,
+}
+
+impl<T, D> PartialEq for Ind<T, D>
+where
+    T: Tree<StaticGraph>,
+    D: VertexPropMut<StaticGraph, u32>,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.hash == other.hash && self.branches == other.branches && self.leafs == other.leafs
+    }
 }
 
 impl<T, D> Ind<T, D>
@@ -174,16 +208,19 @@ where
 {
     fn new_(tree: T, mut degree: D, edges: &[Edge<StaticGraph>]) -> Self {
         let g = tree.graph().clone();
+        let mut hash = 0;
         for &e in edges {
             let (u, v) = g.ends(e);
             degree[u] += 1;
             degree[v] += 1;
+            hash += g.edge_index().get(e);
         }
         let branches = g.vertices().filter(|v| degree[*v] > 2).count() as _;
         let leafs = g.vertices().filter(|v| degree[*v] == 1).count() as _;
         Ind {
             tree,
             degree,
+            hash,
             branches,
             leafs,
         }
@@ -204,6 +241,9 @@ where
             Op::ChangeAny => self.tree.change_any(rng),
             Op::ChangePred => self.tree.change_pred(rng),
         };
+
+        self.hash -= self.tree.graph().edge_index().get(rem);
+        self.hash += self.tree.graph().edge_index().get(ins);
 
         let (a, b) = self.tree.graph().ends(rem);
         if self.degree[a] == 3 {
@@ -244,12 +284,31 @@ where
         (self.branches, self.leafs)
     }
 
-    fn fitness(&self) -> u32 {
-        self.branches
+    fn fitness(&self) -> (u32, u32) {
+        (self.leafs, self.branches)
     }
 
+    fn is_optimum(&self) -> bool {
+        self.branches == 0
+    }
+
+    #[cfg(not(debug_assertions))]
+    fn check(&self) {}
+
+    #[cfg(debug_assertions)]
     fn check(&self) {
-        debug_assert_eq!(
+        use fera::graph::algs::Degrees;
+        let g = self.tree.graph();
+        let edges = self.tree.edges();
+        let deg = g.degree_spanning_subgraph(&edges);
+        for u in g.vertices() {
+            assert_eq!(deg[u], self.degree[u]);
+        }
+        assert_eq!(
+            edges.iter().map(|&e| g.edge_index().get(e)).sum::<usize>(),
+            self.hash
+        );
+        assert_eq!(
             self.tree
                 .graph()
                 .vertices()
@@ -257,7 +316,7 @@ where
                 .count() as u32,
             self.branches
         );
-        debug_assert_eq!(
+        assert_eq!(
             self.tree
                 .graph()
                 .vertices()
@@ -268,7 +327,166 @@ where
     }
 }
 
-#[derive(Copy, Clone)]
+fn path(g: &StaticGraph, edges: &[Edge<StaticGraph>]) -> Vec<Edge<StaticGraph>> {
+    let mut path = vec![];
+    let sub = g.edge_induced_subgraph(edges);
+    let r = sub.vertices().find(|v| sub.out_degree(*v) == 1).unwrap();
+    sub.dfs(OnDiscoverTreeEdge(|e| path.push(e))).root(r).run();
+    assert!(g.is_path(&path));
+    path
+}
+
+fn transform(g: &StaticGraph) -> StaticGraph {
+    let n = g.num_vertices();
+    let v = n - 1;
+    let v_ = n;
+    let s = n + 1;
+    let t = n + 2;
+    let mut builder = StaticGraph::builder(n + 3, 0);
+    for (u, v) in g.ends(g.edges()) {
+        builder.add_edge(u as _, v as _);
+    }
+    for w in g.out_neighbors(v as _) {
+        builder.add_edge(v_, w as _);
+    }
+    builder.add_edge(s, v);
+    builder.add_edge(t, v_);
+    builder.add_edge(v, v_);
+    builder.finalize()
+}
+
+fn transform_path(g: &StaticGraph, path: &mut Vec<Edge<StaticGraph>>) {
+    let n = g.num_vertices() as u32;
+    let t = n - 1;
+    let s = n - 2;
+    let v_ = n - 3;
+    let v = n - 4;
+    let last = path.pop().unwrap();
+    let first = path.remove(0);
+    assert!(s == g.source(first) || t == g.source(first));
+    assert!(s == g.target(last) || t == g.target(last));
+    if v_ == g.source(*path.first().unwrap()) {
+        let x = g.target(*path.first().unwrap());
+        *path.first_mut().unwrap() = g.edge_by_ends(v, x);
+    } else {
+        assert_eq!(v_, g.target(*path.last().unwrap()));
+        let x = g.source(*path.last().unwrap());
+        *path.last_mut().unwrap() = g.edge_by_ends(x, v);
+    }
+}
+
+#[derive(Debug)]
+struct Args {
+    repr: Repr,
+    op: Op,
+    input: String,
+    seed: u32,
+    pop_size: u32,
+    max_num_iters: Option<u64>,
+    max_num_iters_no_impr: Option<u64>,
+    path: bool,
+    quiet: bool,
+}
+
+fn args() -> Args {
+    let app = clap_app!(
+        ("hcp") =>
+            (version: crate_version!())
+            (about: crate_description!())
+            (author: crate_authors!())
+            (@arg seed:
+                --seed
+                +takes_value
+                "The seed used in the random number generator. A random value is used if none is specified")
+            (@arg pop_size:
+                --pop_size
+                +takes_value
+                default_value("10")
+                "The number of individual in the population")
+            (@arg max_num_iters:
+                --max_num_iters
+                +takes_value
+                "The maximum number of iterations")
+            (@arg max_num_iters_no_impr:
+                --max_num_iters_no_impr
+                +takes_value
+                "The maximum number of iterations without improvements")
+            (@arg path: --path "Find a Hamiltonian path instead of a cycle")
+            (@arg quiet: --quiet "Only prints the final solution")
+            (@arg repr:
+                +required
+                possible_values(&[
+                    "euler-tour",
+                    "nddr-adj",
+                    "nddr-balanced",
+                    "pred",
+                    "pred2",
+                ])
+                "tree representation"
+            )
+            (@arg op:
+                +required
+                possible_values(&[
+                    "change-pred",
+                    "change-any"
+                ])
+                "operation"
+            )
+            (@arg input:
+                +required
+                "input file graph (an HCP tsplib instance)"
+            )
+    );
+
+    let matches = app.get_matches();
+
+    Args {
+        seed: value_t!(matches.value_of("seed"), u32).unwrap_or_else(|e| {
+            if e.kind == ErrorKind::ArgumentNotFound {
+                rand::thread_rng().gen()
+            } else {
+                e.exit()
+            }
+        }),
+        pop_size: value_t_or_exit!(matches.value_of("pop_size"), u32),
+        max_num_iters: value_t!(matches.value_of("max_num_iters"), u64)
+            .map(|v| Some(v))
+            .unwrap_or_else(|e| {
+                if e.kind == ErrorKind::ArgumentNotFound {
+                    None
+                } else {
+                    e.exit()
+                }
+            }),
+        max_num_iters_no_impr: value_t!(matches.value_of("max_num_iters_no_impr"), u64)
+            .map(|v| Some(v))
+            .unwrap_or_else(|e| {
+                if e.kind == ErrorKind::ArgumentNotFound {
+                    None
+                } else {
+                    e.exit()
+                }
+            }),
+        path: matches.is_present("path"),
+        quiet: matches.is_present("quiet"),
+        repr: match matches.value_of("repr").unwrap() {
+            "euler-tour" => Repr::EulerTour,
+            "nddr-adj" => Repr::NddrAdj,
+            "nddr-balanced" => Repr::NddrBalanced,
+            "pred" => Repr::Predecessor,
+            "pred2" => Repr::Predecessor2,
+            _ => unreachable!(),
+        },
+        op: match matches.value_of("op").unwrap() {
+            "change-pred" => Op::ChangePred,
+            "change-any" => Op::ChangeAny,
+            _ => unreachable!(),
+        },
+        input: matches.value_of("input").unwrap().to_string(),
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
 enum Repr {
     EulerTour,
     NddrAdj,
@@ -277,7 +495,7 @@ enum Repr {
     Predecessor2,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 enum Op {
     ChangePred,
     ChangeAny,
