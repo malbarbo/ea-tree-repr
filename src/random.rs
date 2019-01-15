92c61220b9b026b591f913dec3a78e4186fb5ac6
Marco A L Barbosa <malbarbo@gmail.com>
2019-01-15 08:42:07 -0200

    Add (not working as expected) random_carrabs and random_cerrone

--- src/random.rs
+++ src/random.rs
@@ -1,8 +1,12 @@
+use fera::ext::VecExt;
+use fera::fun::vec;
 use fera::graph::algs::Kruskal;
 use fera::graph::choose::Choose;
 use fera::graph::prelude::*;
 use rand::prelude::*;
 use rand::XorShiftRng;
+
+use std::collections::HashSet;
 
 pub fn new_rng_with_seed(x: u32) -> XorShiftRng {
     let b1: u8 = ((x >> 24) & 0xff) as u8;
@@ -32,6 +36,109 @@ pub fn random_sp_with_diameter<R: Rng>(
         .edges_ends()
         .map(|(u, v)| g.edge_by_ends(u, v))
         .collect()
+}
+
+
+// FIXME: This does not work as expected
+pub fn random_carrabs<R: Rng>(n: usize, i: usize, mut rng: R) -> StaticGraph {
+    fn sort(a: usize, b: usize) -> (usize, usize) {
+        if a > b {
+            (a, b)
+        } else {
+            (b, a)
+        }
+    }
+    assert!(n > 1);
+    assert!(i > 0);
+    let m = n - 1 + f64::floor(1.5 * (i as f64) * f64::ceil(f64::sqrt(n as f64))) as usize;
+
+    let vertices = vec(0..n).shuffled_with(&mut rng);
+    let mut builder = StaticGraph::builder(n, m);
+    let mut edges = HashSet::new();
+    for i in 1..vertices.len() {
+        let u = *rng.choose(&vertices[0..i]).unwrap();
+        let v = vertices[i];
+        builder.add_edge(u, v);
+        edges.insert(sort(u, v));
+    }
+
+    while edges.len() < m {
+        let u = *rng.choose(&vertices).unwrap();
+        let v = *rng.choose(&vertices).unwrap();
+        if u == v || edges.contains(&sort(u, v)) {
+            continue
+        }
+        builder.add_edge(u, v);
+        edges.insert(sort(u, v));
+    }
+
+    builder.finalize()
+
+    // StaticGraph::new_gnm_connected(n, m, rng).unwrap()
+}
+
+
+// FIXME: This does not work as expected
+pub fn random_cerrone<R: Rng>(n: usize, mut rng: R) -> StaticGraph {
+    fn sort(a: usize, b: usize) -> (usize, usize) {
+        if a > b {
+            (a, b)
+        } else {
+            (b, a)
+        }
+    }
+    assert!(n >= 4);
+    assert!(n % 2 == 0);
+    let vertices = vec(0..n).shuffled_with(&mut rng);
+    let mut builder = StaticGraph::builder(n, 4 * n);
+    let mut edges = HashSet::new();
+    let mut tours: Vec<[usize; 4]> = vec![];
+    let mut a = vertices[0];
+    let mut b = vertices[1];
+    let mut c = vertices[2];
+    let mut d = vertices[3];
+    builder.add_edge(a, b);
+    builder.add_edge(b, c);
+    builder.add_edge(c, d);
+    builder.add_edge(d, a);
+    edges.insert(sort(a, b));
+    edges.insert(sort(b, c));
+    edges.insert(sort(c, d));
+    edges.insert(sort(d, a));
+    tours.push([a, b, c, d]);
+    for i in (4..vertices.len()).step_by(2) {
+        {
+            let tour = rng.choose(&tours).unwrap();
+            a = *rng.choose(tour).unwrap();
+            b = a;
+            while b == a {
+                b = *rng.choose(tour).unwrap();
+            }
+        }
+        c = vertices[i];
+        d = vertices[i + 1];
+        if !edges.contains(&sort(a, b)) {
+            builder.add_edge(a, b);
+            edges.insert(sort(a, b));
+        }
+        builder.add_edge(b, c);
+        builder.add_edge(c, d);
+        builder.add_edge(d, a);
+        edges.insert(sort(b, c));
+        edges.insert(sort(c, d));
+        edges.insert(sort(d, a));
+        tours.push([a, b, c, d]);
+    }
+    let m = edges.len();
+    while edges.len() < (m + n / 2) {
+        let a = *rng.choose(&vertices).unwrap();
+        let b = *rng.choose(&vertices).unwrap();
+        if a != b && !edges.contains(&sort(a, b)) {
+            builder.add_edge(a, b);
+            edges.insert(sort(a, b));
+        }
+    }
+    builder.finalize()
 }
 
 #[derive(Default)]
